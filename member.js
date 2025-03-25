6f4a81029d952ba024785a7d841d3d78d127d393
JhennyLima <31103289+JhennyLima@users.noreply.github.com>
2025-03-25 13:34:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    type: 'member', // member type
+    skills: ['JavaScript', 'React', 'Node.js', 'MongoDB', 'Express.js'],
+  };
+}   
